4bdf7fbea01313cab243af41df5f230fa23bfa38
abhuraya <149016492+abhuraya@users.noreply.github.com>
2024-06-07 10:04:18 -0400

    init commit

---
+++ models/User.js
@@ -0,0 +1,23 @@
+const { Schema, Types } = require('mongoose');
+
+const assignmentSchema  = new Schema (
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: true,
+            trimmed: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            validate: {
+                validator: function(v) {
+                    return /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/.test(v);
+                },
+                message: props => `${props.value} is an invalid email!`
+            }
+        },
+    }
+)
